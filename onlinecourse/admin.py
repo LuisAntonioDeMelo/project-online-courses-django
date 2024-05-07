63ee19d275d56299ff2bd92ea7d32efa23847908
luis antonio <tyluis20@gmail.com>
2024-05-07 01:06:52 -0300

    finalizar projeto

---
+++ onlinecourse/admin.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from .models import Course, Lesson, Instructor, Learner,Question,Choice,Submission
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
---
+++ onlinecourse/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+app_name = 'onlinecourse'
+urlpatterns = [
+    path(route='', view=views.CourseListView.as_view(), name='index'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+    path('<int:pk>/', views.CourseDetailView.as_view(), name='course_details'),
+    path('<int:course_id>/enroll/', views.enroll, name='enroll'),
+    path('<int:course_id>/submit/', views.submit, name="submit"),
+    path('course/<int:course_id>/submission/<int:submission_id>/result/', views.show_exam_result, name="exam_result"),
+ ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
